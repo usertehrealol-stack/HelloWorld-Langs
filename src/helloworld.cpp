82f251f424ae0e7ffc51f549a15995a6927ea907
usertehrealol-stack <usertehrealol@gmail.com>
2026-02-14 19:08:05 +0000

    helloworld.cpp

---
+++ src/helloworld.cpp
@@ -0,0 +1,9 @@
+// Hello World in C++.
+
+#include <iostream> // Standard library for output.
+
+int main() {
+  // Defines where our code will execute.
+  std::cout << "Hello, World!" << endl; // Outputs Hello, World to console. Endl ends the line and starts on a new line.
+  return 0;
+}
