2cc39233d19c8472c4df7817ebb301ae565142eb
usertehrealol-stack <usertehrealol@gmail.com>
2026-02-14 19:04:56 +0000

    helloworld.c

---
+++ src/helloworld.c
@@ -0,0 +1,9 @@
+// Hello World in C
+
+#include <stdio.h> // Standard input output library; used for functions like printing.
+
+int main() {
+   // Entry point for our code; where it will execute.
+  printf("Hello, World!"); // Outprints "Hello, World!"
+  return 0; // Exits the program with success.
+}
